00ad070b14159d3127a2113a45d37d99096bbad0
daigo-tanaka <daigot.2718@gmail.com>
2025-09-04 19:08:30 +0900

    SegmentにdirectionとgetBranchConfigを追加

--- Models/Segment.swift
+++ Models/Segment.swift
@@ -7,6 +7,9 @@ struct Segment: Identifiable {
     var length: Float {
         sqrtf(Float(pow(start.x - end.x, 2.0) + pow(start.y - end.y, 2.0)))
     }
+    var direction: Float {
+        atan2f(Float(end.x-start.x),Float(end.y-start.y))
+    }
     
     init(start: CGPoint, end: CGPoint) {
         self.start = start
@@ -17,7 +20,14 @@ struct Segment: Identifiable {
         self.end = CGPoint(x: endX, y: endY)
     }
     
-    func getBranchConfig() -> BranchConfig {
-        
+    func getBranchConfig(parent:Segment) -> BranchConfig {
+        let startRatio = self.start.getDistance(to:parent.start) / parent.length
+        let startAngle = atan2f(self.start.x-parent.start.x,self.start.y-parent.start.y)-parent.direction
+        let endRatio = self.end.getDistance(to:parent.start) / parent.length
+        let endAngle = atan2f(self.end.x-parent.start.x,self.end.y-parent.start.y)-parent.direction
+
+        return BranchConfig(start: (CGFloat(startRatio),CGFloat(startAngle)),
+        end: (CGFloat(endRatio),CGFloat(endAngle)))
+
     }
 }
